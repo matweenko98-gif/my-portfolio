c8a644baf41f488e90fd561b1459978f218c52e7
matweenko98 <matweenko98@gmail.com>
2026-06-14 14:28:31 +0300

    refactor: remove nested duplicate project, unify configs and fix esbuild vulnerability

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'es2022'
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'es2022'
+    }
+  }
 })
